521d48067b950d202b0d0d2ca1f1e9ac7eedfaed
Anurag Kanase <79215426+anuwrag@users.noreply.github.com>
2024-11-29 02:48:39 -0600

    changed the output channels from 8 to 6.

--- cypher/model.py
+++ cypher/model.py
@@ -4,11 +4,11 @@ class MNISTModel(nn.Module):
     def __init__(self):
         super(MNISTModel, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(1, 8, kernel_size=3),  # 26x26x8
+            nn.Conv2d(1, 6, kernel_size=3),  # 26x26x8
             nn.ReLU(),
             nn.MaxPool2d(2),  # 13x13x8
             
-            nn.Conv2d(8, 16, kernel_size=3),  # 11x11x16
+            nn.Conv2d(6, 16, kernel_size=3),  # 11x11x16
             nn.ReLU(),
             nn.MaxPool2d(2)  # 5x5x16
         )
